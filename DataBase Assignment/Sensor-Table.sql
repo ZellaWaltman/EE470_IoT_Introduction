b6425df52f474e407f80a94552741ee2cf2aba96
ZellaWaltman <Waltmanz@sonoma.edu>
2025-10-05 15:22:34 -0700

    Update Sensor-Table.sql
    Added Comments

--- DataBase Assignment/Sensor-Table.sql
+++ DataBase Assignment/Sensor-Table.sql
@@ -2,6 +2,7 @@ SHOW DATABASES;
 
 USE u551180265_ZellaWaltman;
 
+-- Sensor Register:
 CREATE TABLE sensor_register(
     node_name VARCHAR(10) NOT NULL,
     manufacturer VARCHAR(10) NOT NULL,
@@ -9,19 +10,20 @@ CREATE TABLE sensor_register(
     latitude DECIMAL(15, 12) NOT NULL CHECK (latitude BETWEEN -90 AND 90), -- 15 digits, 12 after decimal
     PRIMARY KEY (node_name)
     );
-       
+ 
+-- Sensor Data: 
 CREATE TABLE sensor_data(
     node_name VARCHAR(10) NOT NULL,
     time_received DATETIME NOT NULL,
-    temperature DECIMAL(6, 3) NOT NULL CHECK (temperature BETWEEN -10 AND 100),
-    humidity DECIMAL(6,3) NOT NULL CHECK (humidity BETWEEN 0 AND 100),
-    FOREIGN KEY (node_name) REFERENCES sensor_register(node_name)
+    temperature DECIMAL(6, 3) NOT NULL CHECK (temperature BETWEEN -10 AND 100), -- Must be between -10 and 100
+    humidity DECIMAL(6,3) NOT NULL CHECK (humidity BETWEEN 0 AND 100), -- Must be between 0 and 100
+    FOREIGN KEY (node_name) REFERENCES sensor_register(node_name) -- Linked with Sensor Register Node Name. Cannot insert data for a node that does not exist in sensor_register table.
 	);
    
 -- Register 5 Different Sensors W/ Manufacturer Names
 INSERT INTO sensor_register (node_name, manufacturer, longitude, latitude) VALUES
-('node_1', 'Texas Instruments', -96.796989, 32.776665),  -- Dallas, TX
-('node_2', 'Bosch Sensortec', 9.120415, 48.521932),  -- Kusterdingen, Germany
+('node_1', 'TI', -96.796989, 32.776665),  -- Dallas, TX
+('node_2', 'Bosch', 9.120415, 48.521932),  -- Kusterdingen, Germany
 ('node_3', 'Honeywell',   -80.841141, 35.223789),  -- Charlotte, NC
 ('node_4', 'SICK AG',   -73.989308,  40.741895),  -- Waldkirch, Germany
 ('node_5', 'Initech',   6.117887, 46.1683113);  -- Plan-les-Ouates, Switzerland
@@ -58,7 +60,8 @@ INSERT INTO sensor_data (node_name, time_received, temperature, humidity) VALUES
 ('node_5', '2022-10-01 12:00:00', 44.35, 38.19),
 ('node_5', '2022-10-01 12:30:00', 72.68, 55.28);
 
-CREATE VIEW sensor_combined AS
+-- Merge data w/ register:
+CREATE OR REPLACE VIEW sensor_combined AS
 SELECT
   sensor_data.node_name,
   sensor_data.time_received,
@@ -69,6 +72,6 @@ SELECT
   sensor_register.latitude
 FROM sensor_data, sensor_register
 WHERE
-	sensor_register.node_name = sensor_data.node_name;
+	sensor_register.node_name = sensor_data.node_name; -- Ensures that no data from the sensor_data table is accepted if the transmitting node is not registered in the sensor_register table by only keeping rows where node_name matches.
 
-SELECT * FROM sensor_combined;
+SELECT * FROM sensor_combined; -- Excecute join where node_names are equal
